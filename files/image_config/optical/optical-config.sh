0c69c19ead188aa50459dab71c075412a3c966ef
jimmy <jimmy.jin@molex.com>
2023-09-20 14:01:17 -0700

    use device specific optical configure file

--- files/image_config/optical/optical-config.sh
+++ files/image_config/optical/optical-config.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-cp /usr/share/sonic/templates/optical_config.j2 /etc/sonic/optical_config.json
-python /usr/local/lib/python3.9/dist-packages/redisdl.py -d 4 -l /etc/sonic/optical_config.json
-
 PLATFORM=${PLATFORM:-`sonic-cfggen -H -v DEVICE_METADATA.localhost.platform`}
 sonic-cfggen -j /usr/share/sonic/device/$PLATFORM/ot-metadata.json --write-to-db
+HWSKU=${HWSKU:-`sonic-cfggen -H -v DEVICE_METADATA.localhost.hwsku`}
+sonic-cfggen -j /usr/share/sonic/device/$PLATFORM/$HWSKU/optical_config.json --write-to-db
