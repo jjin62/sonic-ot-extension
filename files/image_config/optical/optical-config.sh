fe6c680b1037824380dbcd517f1111abfae7038f
Jimmy Jin <jimmy.jin@molex.com>
2024-04-01 16:51:50 -0700

    add platform initial config

--- files/image_config/optical/optical-config.sh
+++ files/image_config/optical/optical-config.sh
@@ -4,3 +4,4 @@ PLATFORM=${PLATFORM:-`sonic-cfggen -d -v DEVICE_METADATA.localhost.platform`}
 sonic-cfggen -j /usr/share/sonic/device/$PLATFORM/ot-metadata.json --write-to-db
 HWSKU=${HWSKU:-`sonic-cfggen -d -v DEVICE_METADATA.localhost.hwsku`}
 sonic-cfggen -j /usr/share/sonic/device/$PLATFORM/$HWSKU/optical_config.json --write-to-db
+sonic-cfggen -j /usr/share/sonic/device/$PLATFORM/$HWSKU/platform_config.json --write-to-db
